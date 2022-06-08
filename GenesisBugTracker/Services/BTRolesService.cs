e24b1909c85713a4e4dd0349704fffe5e7db8b49
nelsonleopold <nelsonleopold@gmail.com>
2022-06-08 08:18:37 -0700

    continued carving in the template on the landing page; added more functionality to RolesService

--- GenesisBugTracker/Services/BTRolesService.cs
+++ GenesisBugTracker/Services/BTRolesService.cs
@@ -2,6 +2,7 @@
 using GenesisBugTracker.Models;
 using GenesisBugTracker.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace GenesisBugTracker.Services
 {
@@ -20,6 +21,89 @@ namespace GenesisBugTracker.Services
             _userManager = userManager;
         }
 
+        #region Add User To Role Async
+        public async Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            try
+            {
+                bool result = (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Get BTRoles Async
+        public async Task<List<IdentityRole>> GetBTRolesAsync()
+        {
+            try
+            {
+                List<IdentityRole> result = new();
+                result = await _context.Roles.ToListAsync();
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Get Role Name By Id Async
+        public async Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            try
+            {
+                IdentityRole? role = _context.Roles.Find(roleId);
+                string result = await _roleManager.GetRoleNameAsync(role!);
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Get User Role Async
+        public async Task<string> GetUserRoleAsync(BTUser user)
+        {
+            try
+            {
+                IEnumerable<string> result = await _userManager.GetRolesAsync(user);
+                return result.FirstOrDefault()!;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Get User Roles Async
+        public async Task<IEnumerable<string>> GetUserRolesAsync(BTUser user)
+        {
+            try
+            {
+                IEnumerable<string> result = await _userManager.GetRolesAsync(user);
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Get Users In Role Async
         public async Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
         {
             try
@@ -35,14 +119,66 @@ namespace GenesisBugTracker.Services
                 throw;
             }
         }
+        #endregion
 
+        #region Get Users Not In Role Async
+        public async Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            try
+            {
+                List<string> userIds = (await _userManager.GetUsersInRoleAsync(roleName)).Select(u => u.Id).ToList();
+                List<BTUser> roleUsers = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
+                List<BTUser> result = roleUsers.Where(u => u.CompanyId == companyId).ToList();
 
-        #region
+                return result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Is User In Role Async
         public async Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
         {
             try
             {
                 bool result = await _userManager.IsInRoleAsync(user, roleName);
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Remove User From Role Async
+        public async Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
+        {
+            try
+            {
+                bool result = (await _userManager.RemoveFromRoleAsync(user, roleName)).Succeeded;
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Remove User From Roles Async
+        public async Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            try
+            {
+                bool result = (await _userManager.RemoveFromRolesAsync(user, roles)).Succeeded;
                 return result;
             }
             catch (Exception)
--- GenesisBugTracker/Services/Interfaces/IBTRolesService.cs
+++ GenesisBugTracker/Services/Interfaces/IBTRolesService.cs
@@ -1,11 +1,20 @@
 ﻿using GenesisBugTracker.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace GenesisBugTracker.Services.Interfaces
 {
     public interface IBTRolesService
     {
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+        public Task<List<IdentityRole>> GetBTRolesAsync();
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+        public Task<string> GetUserRoleAsync(BTUser user);
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
         public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
         public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
-        
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
     }
 }
